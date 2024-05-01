43d27603d003e8e092d6cab9d91b67d420f3b84b
David Ochsner <davidochsner93@gmail.com>
2024-05-01 11:38:41 +0200

    Add global autocrlf and rerere to git

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -2,5 +2,13 @@
   programs.git = {
     enable = true;
     userName = "David Ochsner";
+    extraConfig = {
+      core = {
+        autocrlf = true;
+      };
+      rerere = {
+        enabled = true;
+      };
+    };
   };
 }
