7e9e3413d00edfa360300cf1c51b58bd136cce64
David Ochsner <davidochsner93@gmail.com>
2024-05-10 09:41:01 +0200

    New setup (yoga)

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,16 +4,26 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0cb08e44-8933-44b9-95fc-3dc02f50affb";
+    { device = "/dev/disk/by-uuid/f800c592-5824-47d9-9801-6c15717c1991";
       fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-47e3301b-a192-471b-925a-99eefd6591db".device = "/dev/disk/by-uuid/47e3301b-a192-471b-925a-99eefd6591db";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/A734-85A1";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
@@ -23,8 +33,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp121s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  virtualisation.virtualbox.guest.enable = true;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
