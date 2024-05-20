3a15901b1940fcb485d97e0085d5fe4ab52cdeaf
David Ochsner <davidochsner93@gmail.com>
2024-05-20 10:05:56 +0200

    Add fix-zenzone bash-alias

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -4,5 +4,6 @@
     # TODO: git add && commit before?
     # TODO: relative path in repo?
     rebuild = "sudo nixos-rebuild switch --flake ~/nix-config";
+    fix-zenzone = "systemctl restart bluetooth; sleep 2; bluetoothctl connect AC:BF:71:82:B2:ED";
   };
 }
