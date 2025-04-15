a3a2e592da967c2306916c8e9bf2baa2441a9da6
David Ochsner <davidochsner93@gmail.com>
2025-04-15 16:37:43 +0200

    cleanup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,7 +143,11 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["wheel" "networkmanager" "libvirtd"];
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "libvirtd"
+      ];
     };
   };
 
