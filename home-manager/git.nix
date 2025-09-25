f5c683d20dbc22013035a64562a34a4731d1f576
David Ochsner <davidochsner93@gmail.com>
2025-09-25 19:15:55 +0200

    Default git branch is main

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -2,12 +2,16 @@
   programs.git = {
     enable = true;
     userName = "David Ochsner";
+    # do not set email, so I'm forced to decide per repo
     extraConfig = {
       core = {
         autocrlf = false;
       };
       rerere = {
         enabled = true;
+      };
+      init = {
+        defaultBranch = "main";
       };
     };
   };
