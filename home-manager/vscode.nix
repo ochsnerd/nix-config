04f40d180c8e8257d380b0e75b9e867e0f7ea205
David Ochsner <davidochsner93@gmail.com>
2026-07-24 15:42:10 +0200

    Vscode: dbg-snippet

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -4,8 +4,23 @@
     enable = true;
     package = pkgs.vscode.fhs;
     # package = pkgs.vscodium;  <--- sadly c# extensions are anemic
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+      ];
+      languageSnippets = {
+        csharp = {
+          "Wrap with Debug" = {
+            body = [
+              "Debug(\${TM_SELECTED_TEXT:$1})"
+            ];
+            description = "Wrap selected expression with Debug()";
+            prefix = [
+              "dbg"
+            ];
+          };
+        };
+      };
+    };
   };
 }
